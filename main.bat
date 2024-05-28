1da9f01be80ab12e5fa072a390f62459b3ccf90a
zhq <pwnkeeper@gmail.com>
2024-05-28 19:56:53 +0800

    feat: split build tools and sdk since github has signle file size limit (2GB)

--- main.bat
+++ main.bat
@@ -17,6 +17,5 @@ start /WAIT "" vs_buildtools.exe --nocache --quiet --wait ^
 curl.exe -LO https://github.com/ip7z/7zip/releases/download/22.01/7z2201.exe
 7z2201.exe /S /D="%CD%\7z"
 
-xcopy /E "%ProgramFiles(x86)%\Windows Kits" %CD%\ms_buildtools\2022\ >NUL
-
-"%CD%\7z\7z.exe" -tzip ms_buildtools.zip %CD%\ms_buildtools >NUL
+"%CD%\7z\7z.exe" a -tzip ms_buildtools.zip %CD%\ms_buildtools >NUL
+"%CD%\7z\7z.exe" a -tzip ms_buildtools_sdk.zip "%ProgramFiles(x86)%\Windows Kits" >NUL
